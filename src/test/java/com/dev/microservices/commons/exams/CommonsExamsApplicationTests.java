b4ad5aafbb4df5a353fb205ca66788aadb1afc47
Erick Estrada <erickestradapj@gmail.com>
2021-12-04 16:55:32 -0500

    starting project

---
+++ src/test/java/com/dev/microservices/commons/exams/CommonsExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.microservices.commons.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsExamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
